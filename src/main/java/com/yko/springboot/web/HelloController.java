da10a5827a5832c4a09ca521c9922e2fdef88c1b
yko <hiaro711@gmail.com>
2022-10-03 16:43:21 +0900

    v1 commit

---
+++ src/main/java/com/yko/springboot/web/HelloController.java
@@ -0,0 +1,20 @@
+package com.yko.springboot.web;
+
+import com.yko.springboot.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "good";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+}
